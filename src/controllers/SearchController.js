81b2b119d497c07a086158fbca6d0c831fc1aa51
Alan Viana <alanlviana@hotmail.com>
2020-02-20 22:19:23 -0300

    Alterada forma de consulta para que seja retornada mais de um registro.

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -4,7 +4,7 @@ module.exports = {
     async index(request, response){
         const {title} = request.query;
         const OMDB_KEY = process.env.OMDB_KEY;
-        const apiResponse = await axios.get(`http://www.omdbapi.com/?t=${title}&type=movie&apikey=${OMDB_KEY}`);
+        const apiResponse = await axios.get(`http://www.omdbapi.com/?s=${title}&type=movie&apikey=${OMDB_KEY}`);
         response.json(apiResponse.data);
     }
 }
